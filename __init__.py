85e90597f234a1126be1ca8c9f6534be7bfbaef7
povmaniaco <thebountyrenderer@gmail.com>
2017-01-19 23:43:40 +0100

    Apply next mode for search bin path and load plugins.

--- __init__.py
+++ __init__.py
@@ -34,39 +34,10 @@ import sys
 import os
 import ctypes
 
+BIN_PATH = os.environ['BOUNTY_ROOT'] # os.path.join(__path__[0], 'bin')
+PLUGIN_PATH = BIN_PATH +'/plugins' #os.path.join(__path__[0], 'bin', 'plugins')
+sys.path.insert(0,BIN_PATH)
 
-PLUGIN_PATH = os.path.join(__path__[0], 'bin', 'plugins')
-BIN_PATH = os.path.join(__path__[0], 'bin')
-sys.path.append(BIN_PATH)
-
-#---------------------------------------------------------------
-# The order of libs is very important. Please do not alter it.
-#---------------------------------------------------------------
-if sys.platform == 'win32':
-    for file in os.listdir(BIN_PATH):
-        # load dll's from a MSVC build's
-        if file in {'yafaraycore.dll'}:
-            dllArray = ['zlib', 'libiconv', 'libpng16', 'jpeg8', 'libtiff', 'libxml2', 'Half', 'Iex', \
-                        'Imath', 'IlmThread', 'IlmImf', 'yafaraycore', 'yafarayplugin']
-            break
-        # load dll's from a GCC build's
-        else:
-            dllArray = ['libzlib', 'libiconv-2', 'libxml2', 'libjpeg-8', 'libpng16', 'libtiff-5', \
-                        'libfreetype', 'libHalf', 'libIex', 'libIlmThread', 'libImath', \
-                        'libIlmImf', 'libyafaraycore', 'libyafarayplugin']
-
-elif sys.platform == 'darwin':
-    dllArray = ['libyafaraycore.dylib', 'libyafarayplugin.dylib']
-
-else: # linux
-    dllArray = ['libyafaraycore.so', 'libyafarayplugin.so']
-
-# load libraries
-for dll in dllArray:
-    try:
-        ctypes.cdll.LoadLibrary(os.path.join(BIN_PATH, dll))
-    except Exception as e:
-        print("ERROR: Failed to load library {0}, {1}".format(dll, repr(e)))
 
 #--------------------------
 # import exporter modules
